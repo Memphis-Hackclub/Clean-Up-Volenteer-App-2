405c0c17fcdbaf4620d5d0097025fb0cb07a8bfa
Philip Pounds <philipstudentemail@gmail.com>
2021-06-02 11:34:02 -0500

    In this commit I added a working file system which checks for duplicates

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
 <?php
-$htmlfile = fopen("testfile.html", "w")
+
+$listing_status = "Please wait. Your listing is generating.";
+$title = $_GET['title'];
+$price = $_GET['price'];
+$description = $_GET['description'];
+
+$html_contents = 
+<<<HTML
+<html>
+<head>
+    <title>$title</title>
+</head>
+<body>
+    <h1>$title</h1>
+    <p>$price</p>
+    <p>$description</p>
+</body>
+</html>
+HTML;
+
+
+$file_name = "listings/".str_replace(' ', '', $title).".html";
+while (file_exists($file_name)) {
+    $rand_num = rand(100000,999999);
+    $file_name = "listings/".str_replace(' ', '', $title).$rand_num.".html";
+}
+
+
+$htmlfile = fopen($file_name, "w") or die("An error has occured");
+
+
+fwrite($htmlfile, $html_contents);
+
+$listing_status = "Congrats your listing is now live <a href='".$file_name."' target='_blank'>here</a>";
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Finalizing Your Listing...
+</title>
+</head>
+<p><?php echo $listing_status;?></p>
+
+
+</form>
+</html>
