cad6c857da991a0259e8a075723dfe5851d24f95
Philip Pounds <philipstudentemail@gmail.com>
2021-06-02 16:48:22 -0500

    added the paypal function

--- index.php
+++ index.php
@@ -6,6 +6,27 @@ $price = $_GET['price'];
 $description = $_GET['description'];
 $paypal_email = $_GET['paypal'];
 
+function makePaypalButton() {
+    $paypal_button = '
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_xclick">
+        <input type="hidden" name="business" value="'.$paypal_email.'">
+        <input type="hidden" name="lc" value="US">
+        <input type="hidden" name="item_name" value="'.$title.'">
+        <input type="hidden" name="amount" value="'.$price.'">
+        <input type="hidden" name="button_subtype" value="services">
+        <input type="hidden" name="no_note" value="0">
+        <input type="hidden" name="currency_code" value="USD">
+        <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>';
+    return $paypal_button;
+  }
+
+$payment_button = makePaypalButton();
+
+
 $html_contents = 
 <<<HTML
 <html>
@@ -16,6 +37,7 @@ $html_contents =
     <h1>$title</h1>
     <p>$price</p>
     <p>$description</p>
+    $payment_button
 </body>
 </html>
 HTML;
