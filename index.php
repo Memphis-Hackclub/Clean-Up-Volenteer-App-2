0eb3eb0da13325a352ef942f62a6f11b9acfdac0
Philip Pounds <philipstudentemail@gmail.com>
2021-06-03 09:49:49 -0500

    In this commit images are now rendered to the webpage

--- index.php
+++ index.php
@@ -38,24 +38,6 @@ $payment_button = makePaypalButton();
 
 
 
-
-
-$html_contents = 
-<<<HTML
-<html>
-<head>
-    <title>$title</title>
-</head>
-<body>
-    <h1>$title</h1>
-    <p>$price</p>
-    <p>$description</p>
-    $payment_button
-</body>
-</html>
-HTML;
-
-
 $file_path = "listings/".str_replace(' ', '', $title);
 
 $file_name = $file_path.".html";
@@ -69,11 +51,81 @@ while (file_exists($file_name)) {
 $structure = './listingimages/'.$file_path;
 
 
+
 if (!mkdir($structure, 0777, true)) {
     die('An unsual error has occured');
 }
 
+$structure = $structure."/";
 
+
+
+$target_dir = $structure;
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+
+$target_file = ".".$target_file;
+
+$html_contents = 
+<<<HTML
+<html>
+<head>
+    <title>$title</title>
+</head>
+<body>
+    <h1>$title</h1>
+    <img src="$target_file" width="400" height="600">
+    <p>$price</p>
+    <p>$description</p>
+    $payment_button
+</body>
+</html>
+HTML;
 
 $htmlfile = fopen($file_name, "w") or die("An error has occured");
 
