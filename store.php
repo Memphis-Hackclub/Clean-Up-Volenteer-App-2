87f7b653e4e6fcc9d2e4674e2ebc734f3ca12764
Philip Pounds <philipstudentemail@gmail.com>
2021-06-04 11:29:45 -0500

    In this commit I am reading the listing data to display on the page

--- store.php
+++ store.php
@@ -8,7 +8,38 @@ $dir = "listings/";
 if (is_dir($dir)){
   if ($dh = opendir($dir)){
     while (($file = readdir($dh)) !== false){
+      
       echo "filename:" . $file . "<br>";
+      
+      $file = str_replace('.html', '', $file);
+      $dir2 = "listingimages/listings/".$file."/";
+      //echo "<br>".$dir2."</br>";
+
+      $title_file_name = $dir2."title.txt";
+      $price_file_name = $dir2."price.txt";
+  
+
+
+
+      if($file != "."){
+        if($file != ".."){
+          
+          $title_file = fopen($title_file_name , "r") or die("Hmm thats messed up");
+          $title = fgets($title_file);
+          fclose($title_file);
+          $price_file = fopen($price_file_name , "r") or die("Hmm thats messed up");
+          $price = fgets($price_file);
+          fclose($price_file);
+
+        }
+      }
+      
+        
+      
+      
+      
+
+
     }
     closedir($dh);
   }
