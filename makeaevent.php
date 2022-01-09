a42ad2b621ed16793041ff0e1dfa1e681cd58f89
Philip Pounds <philipstudentemail@gmail.com>
2022-01-09 17:02:31 -0600

    In this commit I rearranged the pages so that the event posting tool could work

--- makeaevent.php
+++ makeaevent.php
@@ -1,108 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>
-Make an Event
-</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <center>
-  <br>
-<form action="/add" method="POST"><br>
-<h1>Make a Clean Up Event</h1><br>
-<label>Event Title<br><input maxlength="200" id="title" name="title"><br></label>
-<label>Event Description<br> <textarea maxlength="500" id="description" name="description" rows="4" cols="50"></textarea><br></label>
-<label>How many volenteers?<br><input type="text" maxlength="200" id ="volenteers" name="volenteers"><br></label>
- <label>Date and Time<br><input type="text" maxlength="200" id="date" name="date"><br></label>
- <label>Contact Info<br><input type="text" maxlength="200" id="contact" name="contact"><br></label>
-
- <h1>Event Location Details</h1>
- <label>Country<br><input type="text" maxlength="200" id="country" name="country"></label><br>
- <label>State/Providence/Territory<br><input type="text" maxlength="200" id="state" name="state"></label><br>
- <label>Event Address<br><input type="text" maxlength="200" id="address"name="address"></label><br>
- <p>Or use your location</p>
-   <input id="map" type="checkbox"  name="map" value="true" checked>
-   <label for="map">Use my location, I am at the clean up site.</label><br><br>
-   <br><label>Your location only used if you consent above</label><br>
-   <textarea maxlength="500" id="loc" name="loc" rows="1" cols="50">Not Given</textarea><br>
- <label><input type="submit" id="list" name="submit" value="Add Your Event"></label> 
- <br>
-
- <br>
- </form>
-
-<h1>Event Location Details</h1>
-     </form> 
- <a href="/">View Other Events</a>
- </center>
- <p id='test'></p>
-  <p id='loc2'></p>
- </body>
- <script>
-
-
-     document.getElementById('loc2').innerHTML = "🥶";//String(urlu);
-function locationSuccess(position) {
-    latitude = String(position.coords.latitude);
-    longitude = String(position.coords.longitude);
-    display(latitude,longitude)
-    
-  
-    
-        
-        
-}
-
-function locationError(error) {
-    const code = error.code;
-    const message = error.message;
-
-    }
-
-navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-
-function display(lat, long){
-  urlu = "{ lat: "+lat+", lng: "+long+" }";
-  document.getElementById('loc').innerHTML = urlu;//String(urlu);
-//<input type="checkbox" id ="map" name="map" value="'+urlu+'" checked>';
-
-}
- </script>
- <script src="js/script.js">
- </script>
- </html>
-
  <?php
 
 
 
 $listing_status = "Please wait. Your listing is generating.";
 $title = $_POST['title'];
-$price = $_POST['price'];
+$vol = $_POST['volenteers'];
 $description = $_POST['description'];
-$paypal_email = $_POST['paypal'];
+$date = $_POST['date'];
+$contact = $_POST['contact'];
+$country = $_POST['country'];
+$state = $_POST['state'];
+$address = $_POST['address'];
+$map = $_POST['map'];
+$loc = $_POST['loc'];
 
-function makePaypalButton() {
-    $paypal_button = '
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-        <input type="hidden" name="cmd" value="_xclick">
-        <input type="hidden" name="business" value="'.$paypal_email.'">
-        <input type="hidden" name="lc" value="US">
-        <input type="hidden" name="item_name" value="'.$title.'">
-        <input type="hidden" name="amount" value="'.$price.'">
-        <input type="hidden" name="button_subtype" value="services">
-        <input type="hidden" name="no_note" value="0">
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>';
-    return $paypal_button;
-  }
 
 function makeFileName() {
     $file_name = "listings/".str_replace(' ', '', $title).".html";
@@ -114,7 +25,7 @@ function makeFileName() {
 
 }
 
-$payment_button = makePaypalButton();
+
 
 
 
@@ -202,7 +113,7 @@ $html_contents =
     <img src="$target_file" width="600" height="400">
     <p>$price</p>
     <p>$description</p>
-    $payment_button
+    
 </body>
 </html>
 HTML;
